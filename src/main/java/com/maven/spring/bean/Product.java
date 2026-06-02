0d994a8da32c899683de757ba541439c193c0b31
swajal14 <swajalkumarkachhi@gmail.com>
2026-06-02 15:56:41 +0530

    feat: bean is added

--- src/main/java/com/maven/spring/bean/Product.java
+++ src/main/java/com/maven/spring/bean/Product.java
@@ -1,4 +1,28 @@
 package com.maven.spring.bean;
 
 public class Product {
+    private int id;
+    private String name;
+    private String brand;
+    private int price;
+    private int manufactureYear;
+
+    public Product(int id, String name, String brand, int price, int manufactureYear) {
+        this.id = id;
+        this.name = name;
+        this.brand = brand;
+        this.price = price;
+        this.manufactureYear = manufactureYear;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", brand='" + brand + '\'' +
+                ", price=" + price +
+                ", manufactureYear=" + manufactureYear +
+                '}';
+    }
 }
